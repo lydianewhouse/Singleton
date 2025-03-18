dc348e262c10f78ee08897873c5402a4bdf5156c
lydianewhouse <lydianewhouse@vt.edu>
2025-03-18 18:21:44 -0400

    Added files

---
+++ db.cpp
@@ -0,0 +1,128 @@
+//db.cpp
+#include "db.h"
+
+//Initializes to be nullptr the first time
+Database* Database::instance = nullptr;
+
+Database::Database(std::string input_db, std::string input_username, std::string input_password)
+{
+    //Initializes all variables
+    db = input_db;
+    username = input_username;
+    password = input_password;
+    connected = true;
+}
+
+Database::~Database()
+{
+    connected = false;
+}
+
+Database* Database::getInstance(std::string input_database, std::string input_username, std::string input_password)
+{
+    //Check if instance doesn't exist, if so, create instance
+    if (instance == nullptr)
+    {
+        //Creates space for instance
+        instance = new Database(input_database, input_username, input_password);
+
+        return instance;
+    }
+    else
+    {
+        //Check if inputs match the existing instance
+        if (input_database == instance->get_db() && input_username == instance->get_username() && input_password == instance->get_password())
+        {
+            return instance;
+        }
+        else
+        {
+            //Invalid arguments
+            throw std::runtime_error("invalid database name, usernae, or password");
+        }
+    }
+}
+
+
+void Database::connect()
+{
+    connected = true;
+}
+
+void Database::disconnect()
+{
+    connected = false;
+}
+
+bool Database::is_connected()
+{
+    return connected;
+}
+
+
+void* Database::operator new(size_t size)
+{
+    //Creates new database with malloc
+    std::cout << " overloaded new " << std::endl;
+    void * new_database = malloc(size);
+
+    //Check if unable to allocate
+    if (new_database == NULL)
+    {
+        throw std::bad_alloc();
+    }
+
+    return new_database;
+}
+
+void Database::operator delete(void* new_database)
+{
+    std::cout << " overloaded delete " << std::endl;
+
+    free(new_database);
+}
+
+
+
+
+void Database::set_username(std::string new_username)
+{
+    username = new_username;
+}
+
+
+
+std::string Database::get_username()
+{
+    return username;
+}
+
+void Database::set_password(std::string new_password)
+{
+    password = new_password;
+}
+
+std::string Database::get_password()
+{
+    return password;
+}
+
+void Database::set_db(std::string new_db)
+{
+    db = new_db;
+}
+
+std::string Database::get_db()
+{
+    return db;
+}
+
+
+void Database::resetInstance() {
+    if (instance != nullptr) {
+        delete instance;
+        instance = nullptr;
+    }
+}
+
+
---
+++ db.h
@@ -0,0 +1,75 @@
+//db.h
+#pragma once
+#include <string>
+#include <iostream>
+#include <stdexcept>
+
+
+class Database{
+
+    private:
+    //member variable "db" is of string type
+    //ToDo
+        std::string db;
+    //member variable "username" is of string type
+    //ToDo
+        std::string username;
+    //member variable "password" is of string type
+    //ToDo
+        std::string password;
+    //member variable "connected" is of boolean type
+    //The "connected" is set to false initially.
+    //ToDo
+        bool connected = false;
+    //member variable "instance" is static 
+    //"instance" (pointer to Database) is a staic variable that stores the instance of the database. Its value is set in the function "getInstance"
+    //ToDo
+        static Database* instance;
+    //constructor that helps creating instance of db(e.g. sales.db) accepts name of the database, username, password. 
+    //ToDo
+        Database(std::string input_db, std::string input_username, std::string input_password);
+    //destructor that disconnects the connection if connected.
+    //ToDo
+        ~Database();
+
+    public:
+    //"getInstance" that creates and returns the instance of the database. If called first time it sets the username and password. 
+    //However, subsequent time, it matches the database name, username and password and returns the previous instance if matched else it 
+    //throws std::runtime_error("invalid database name, username or password"). We are using Singleton Design Pattern that creates only one 
+    //instance of the databse. The instance is still created by the constructor.
+    //ToDo
+        static Database* getInstance(std::string input_database, std::string input_username, std::string input_password);
+    //"connect" that sets "connected" to true (return void)
+    //ToDo
+        void connect();
+    //"disconnect" that sets "connected" to false (return void)
+    //ToDo
+        void disconnect();
+    // retrun status of connected true/false (return bool)
+    //ToDo
+        bool is_connected();
+    //overload the new operator that allocates memory using malloc of given size and returns pointer of type void and prints " overloaded new " (cout is okay in this case). std::cout << "overloaded new ";
+    //If the memory allocation fails it should throw std::bad_alloc()
+    //ToDo
+        void* operator new(size_t size);
+    //overload the delete operator that deallocates memory and prints "overloaded delete " (cout is okay in this). std::cout << "overloaded delete ";
+    //ToDo
+        void operator delete(void* instance);
+    //set_username and get_username for username
+    //ToDo
+    //ToDo
+        void set_username(std::string new_username);
+        std::string get_username();
+    //set_password and get_password for password.
+    //ToDo
+    //ToDo
+        void set_password(std::string new_password);
+        std::string get_password();
+
+    //set_db and get_db for database name
+    void set_db(std::string new_db);
+    std::string get_db();
+    
+    //The static "resetInstance" as defined below.
+        static void resetInstance();
+};
